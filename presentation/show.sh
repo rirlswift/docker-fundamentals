0705b712828f63cd71726b7d981ad1fd74fe23f0
rlandon <rlandon@swiftengineering.com>
2021-06-28 15:55:56 +0000

    DevOps: Increment to v2 of RevealJS image. DOIDAC-32 #time 1h

--- presentation/show.sh
+++ presentation/show.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Helper to stop RevealJS in container.
+# Helper to copy presentation content and slides into the server.
 set -x
 docker cp  publish/images swift-presentation-server:/reveal.js/images
 docker cp  publish/docker-fundamentals.html swift-presentation-server:/reveal.js/index.html
--- presentation/start.sh
+++ presentation/start.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bas h
 # Helper to start RevealJS in container.
 set -x
-docker run -it --rm -p 8000:8000 --name "swift-presentation-server" --entrypoint /bin/bash  --workdir=/reveal.js swiftx/revealjs:v1 -c "npm start"
+docker run -it --rm -p 8000:8000 --name "swift-presentation-server" --entrypoint /bin/bash  --workdir=/reveal.js swiftx/revealjs:v2 -c "npm start"
 set +x 
