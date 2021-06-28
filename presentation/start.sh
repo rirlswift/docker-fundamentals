23aa91d8baf55934858fabd10d3a1bf9c1252e66
rlandon <rlandon@swiftengineering.com>
2021-06-28 16:19:15 +0000

    DevOps: Increment RevealJs image tag. #time 1h

--- presentation/start.sh
+++ presentation/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas h
+#!/usr/bin/env bash
 # Helper to start RevealJS in container.
 set -x
 docker run -it --rm -p 8000:8000 --name "swift-presentation-server" --entrypoint /bin/bash  --workdir=/reveal.js swiftx/revealjs:v2 -c "npm start"
